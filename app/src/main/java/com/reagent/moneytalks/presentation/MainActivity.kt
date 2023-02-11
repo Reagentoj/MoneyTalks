0ee531e0b123ce97f7d57a8c176ab293254e3146
reagent <reagentoj@gmail.com>
2023-02-11 18:04:01 +0300

    packages added

---
+++ app/src/main/java/com/reagent/moneytalks/presentation/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.reagent.moneytalks.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.reagent.moneytalks.R
+
+class MainActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
--- feature_cryptocurrencies/src/main/java/com/example/feature_cryptocurrencies/presentation/CryptocurrenciesFragment.kt
+++ feature_cryptocurrencies/src/main/java/com/example/feature_cryptocurrencies/presentation/CryptocurrenciesFragment.kt
@@ -1,19 +1,20 @@
-package com.example.feature_splashscreen
+package com.example.feature_cryptocurrencies.presentation
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.feature_cryptocurrencies.R
 
-class SplashFragment : Fragment() {
 
-
+class CryptocurrenciesFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_splash, container, false)
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_cryptocurrencies, container, false)
     }
 
 }
