8acc8e04e687516d96beee77a65891a63c8b6bc4
reagent <reagentoj@gmail.com>
2023-02-20 14:58:11 +0300

    navigation code refactoring (MainActivity)

--- app/src/main/java/com/reagent/moneytalks/presentation/MainActivity.kt
+++ app/src/main/java/com/reagent/moneytalks/presentation/MainActivity.kt
@@ -2,6 +2,8 @@ package com.reagent.moneytalks.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -9,15 +11,19 @@ import com.reagent.moneytalks.R
 
 class MainActivity : AppCompatActivity() {
 
+    private val navHostFragment by lazy {
+        supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
-        val navController = navHostFragment.navController
-
-        findViewById<BottomNavigationView>(R.id.bottom_nav)
-            .setupWithNavController(navController)
+        setupNavigation()
 
     }
+
+    private fun setupNavigation() {
+        findViewById<BottomNavigationView>(R.id.bottom_nav)
+            .setupWithNavController(navHostFragment.navController)
+    }
+
 }
